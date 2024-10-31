ee1b2c6ae517b5b91127ade4495aed2d1bca28e1
MDOne <llapotkovs@yandex.ru>
2024-10-31 12:55:05 +0300

    commit

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -5,17 +5,17 @@ public class Main {
     public static void main(String[] args) {
         UserServiceImpl userService = new UserServiceImpl();
 
-        userService.createUsersTable();
+//        userService.createUsersTable();
 
-        userService.saveUser("name_1", "las_name_1", (byte) 20);
-        userService.saveUser("name_2", "las_name_2", (byte) 30);
-        userService.saveUser("name_3", "las_name_3", (byte) 40);
+        userService.saveUser("name_1", "last_name_1", (byte) 20);
+        userService.saveUser("name_2", "last_name_2", (byte) 30);
+        userService.saveUser("name_3", "last_name_3", (byte) 40);
 
-        userService.removeUserById(2);
+//        userService.removeUserById(2);
 
-        userService.getAllUsers();
+//        userService.getAllUsers();
 
-        userService.cleanUsersTable();
-        userService.dropUsersTable();
+//        userService.cleanUsersTable();
+//        userService.dropUsersTable();
     }
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,8 +1,12 @@
 package jm.task.core.jdbc.util;
+import jm.task.core.jdbc.model.User;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+
 
 public class Util {
     private static final String url = "jdbc:mysql://localhost:3306/kata_academy";
@@ -10,8 +14,18 @@ public class Util {
     private static final String password = "root";
 
     private static final Connection connection;
+    private static final SessionFactory sessionFactory;
 
     static {
+        sessionFactory = new Configuration()
+                .addAnnotatedClass(User.class)
+                .setProperty("hibernate.connection.url", "jdbc:mysql://localhost:3306/kata_academy")
+                .setProperty("hibernate.connection.driver_class", "com.mysql.cj.jdbc.Driver")
+                .setProperty("hibernate.connection.username", "root")
+                .setProperty("hibernate.connection.password", "root")
+                .setProperty("hibernate.dialect", "org.hibernate.dialect.MySQLDialect")
+                .buildSessionFactory();
+
         try {
             connection = DriverManager.getConnection(url, user, password);
         } catch (SQLException e) {
@@ -21,5 +35,9 @@ public class Util {
 
     public static Connection getConnection() {
         return connection;
+    }
+
+    public static SessionFactory getSessionFactory() {
+        return sessionFactory;
     }
 }
